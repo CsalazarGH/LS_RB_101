e0af5b9d832731c669bed4686642618f71b71644
Chris Salazar <chrissalazarem@icloud.com>
2020-04-09 15:03:53 -0700

    Lesson 2 Upload

---
+++ Lesson_2/1st_Calculator.rb
@@ -0,0 +1,21 @@
+Kernel.puts("Please enter your first number:")
+number1 = Kernel.gets().chomp().to_i
+
+Kernel.puts("Please enter your second number:")
+number2 = Kernel.gets().chomp().to_i
+
+Kernel.puts("Please enter the type of operation to perform.")
+Kernel.puts("Enter add, subtract, multiply or divide:")
+
+operation = Kernel.gets().chomp().downcase
+
+case operation
+when 'add'
+  Kernel.puts("#{number1} + #{number2} = #{number1 + number2}")
+when 'subtract'
+  Kernel.puts("#{number1} - #{number2} = #{number1 - number2}")
+when 'multiply'
+  Kernel.puts("#{number1} * #{number2} = #{number1 * number2}")
+else
+  Kernel.puts("#{number1} / #{number2} = #{number1.to_f / number2.to_f}")
+end
