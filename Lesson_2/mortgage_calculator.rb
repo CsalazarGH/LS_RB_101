6f584f554de797c4f275b4cf6e9572e790918732
Chris Salazar <chrissalazarem@icloud.com>
2020-04-13 12:10:38 -0700

    calculator edits

--- Lesson_2/mortgage_calculator.rb
+++ Lesson_2/mortgage_calculator.rb
@@ -7,8 +7,20 @@ def prompt(string) # adds => to string
   Kernel.puts('=> ' + string)
 end
 
-def valid_input(str) # Validates inputs
-  !str.empty? && str.to_f > 0 && !str.match(/A-Za-z/)
+def valid_amount?(str) # Validates inputs
+  !str.empty? && str.to_f > 0 && (str.match(/[A-Za-z]/) ? true : false) == false
+end
+
+def valid_apr(str)
+  !str.empty? && (str.match(/[A-Za-z]/) ? true : false) == false && str.to_f.between?(0, 99)
+end
+
+def valid_duration(str)
+  !str.empty? && (str.match(/[A-Za-z]/) ? true : false) == false && str.to_i > 0 && str.to_i == str.to_fs 
+end
+
+def valid_inp?(str)
+  str == 'y' || str == 'n'
 end
 
 prompt(MESSAGES['welcome_part1'])
@@ -20,7 +32,7 @@ loop do # main loop
   loop do # collect loan amount and validate input
     prompt(MESSAGES['loan_amount_input'])
     loan_amount = gets.chomp()
-    break if valid_input(loan_amount)
+    break if valid_amount?(loan_amount)
     prompt(MESSAGES['not_valid_input'])
   end
 
@@ -29,7 +41,7 @@ loop do # main loop
   loop do # collect apr and validate input
     prompt(MESSAGES['apr_input'])
     apr = gets.chomp()
-    break if valid_input(apr)
+    break if valid_apr(apr)
     prompt(MESSAGES['not_valid_input'])
   end
 
@@ -38,18 +50,21 @@ loop do # main loop
   loop do # collect duration of years
     prompt(MESSAGES['duration_input'])
     duration = gets.chomp()
-    break if valid_input(duration)
+    break if valid_duration(duration)
     prompt(MESSAGES['not_valid_input'])
   end
 
   loan_amount_float = loan_amount.to_f() # convert loan amount to a float
   month_interest_rate = apr.to_f() / 12 / 100 # convert to monthly interest rate
-  months_duration = duration.to_f() * 12 # convert years to months
+  months_duration = duration.to_i() * 12 # convert year to months
 
   # calculate the monthly mortgage payment
-  monthly_payment = loan_amount_float *
-                    (month_interest_rate /
-                    (1 - (1 + month_interest_rate)**(-months_duration)))
+  monthly_payment = if apr.to_f == 0
+                      loan_amount_float / months_duration
+                    else
+                      loan_amount_float * (month_interest_rate /
+                      (1 - (1 + month_interest_rate)**(-months_duration)))
+                    end
 
   result_message = <<-MSG
   Your monthly payment would be
@@ -59,10 +74,18 @@ loop do # main loop
   prompt(MESSAGES['calculating'])
   prompt(result_message)
 
-  prompt(MESSAGES['calc_again']) # ask if they would like to calculate again
-  input = gets.chomp().downcase()
+  input = nil
 
-  if input == 'n'
+  loop do
+    prompt(MESSAGES['calc_again']) # ask if they would like to calculate again
+    input = gets.chomp().downcase()
+    break if valid_inp?(input)
+    prompt(MESSAGES['calc_again_error'])
+  end
+
+  if input == 'y'
+    system('clear') || system('cls')
+  else
     prompt(MESSAGES['powering_off'])
     break
   end
