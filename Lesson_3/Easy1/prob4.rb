7c8756ee5da50dbeafcc48f11de3e651aa6b5fc3
Chris Salazar <chrissalazarem@icloud.com>
2020-04-16 17:32:01 -0700

    Problem 4

---
+++ Lesson_3/Easy1/prob4.rb
@@ -0,0 +1,6 @@
+# The Ruby Array class has several methods for removing items from the array.
+# Two of them have very similar names. Let's see how they differ:
+
+numbers = [1, 2, 3, 4, 5]
+
+p numbers.delete(1)
