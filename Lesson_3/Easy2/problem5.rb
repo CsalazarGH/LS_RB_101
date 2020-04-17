46af318fc4a6989d27914ad8b7c23e0c97283b38
Chris Salazar <chrissalazarem@icloud.com>
2020-04-16 18:32:47 -0700

    Problem 5

---
+++ Lesson_3/Easy2/problem5.rb
@@ -0,0 +1,7 @@
+# Whats an easier way to write out the flintstones hash?
+
+flintstones = ["Fred", "Barney", "Wilma", "Betty", "BamBam", "Pebbles"]
+
+
+flintstones = %W(Fred Barney Wilma Betty BamBam Pebbles)
+# => ["Fred", "Barney", "Wilma", "Betty", "BamBam", "Pebbles"]
