846f0dc9c6506a2abeecfd1239b772322a5ada71
Chris Salazar <chrissalazarem@icloud.com>
2020-04-30 17:19:14 -0700

    Easy 4 Problem 2

--- Small_Problems/SP_Easy4/E4_2.rb
+++ Small_Problems/SP_Easy4/E4_2.rb
@@ -1,26 +1,36 @@
-def century_suffix(digit)
-  hash = {"1" => 'st',
-          "2" => 'nd',
-          "3" => 'rd',
-          "4" => 'th',
-          "5" => 'th',
-          "6" => 'th',
-          "7" => 'th',
-          "8" => 'th',
-          "9" => 'th',
-          "0" => 'th'}
-          
-  if [11, 12, 13].include?(digit % 100)
+=begin
+Problem: Define a method that takes a integer as an argument (a year).  
+         The method should return the correct century along with its suffix.
+         Example : Century(2000) = 20th century, Century(256) = 3rd century.
+Steps: - First calculate the century of the year given
+       - Attatch the suffix that would go with that century integer.
+       - Return the century with appended suffix
+=end
+
+HASH = {0 => 'th',
+        1 => 'st',
+        2 => 'nd',
+        3 => 'rd',
+        4 => 'th',
+        5 => 'th',
+        6 => 'th',
+        7 => 'th',
+        8 => 'th',
+        9 => 'th', }
+
+def add_sufix(century)
+  exceptions = [11, 12, 13]
+  if exceptions.include?(century % 100)
     'th'
   else
-    suffix = hash[digit.to_s[-1]]
+    HASH[century.to_s[-1].to_i]
   end
 end
 
 def century(year)
-  century = year / 100 + 1
+  century = (year / 100) + 1 
   century -= 1 if year % 100 == 0
-  century.to_s + century_suffix(century)
+  string = century.to_s << add_sufix(century)
 end
 
 p century(2000) == '20th'
