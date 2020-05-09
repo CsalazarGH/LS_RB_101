f1ee3b461b90f71884521b1aff822f3d71f86b74
Chris Salazar <chrissalazarem@icloud.com>
2020-05-08 21:39:43 -0700

    Problem 8 refactored

--- Small_Problems/SP_Easy7/E7_8.rb
+++ Small_Problems/SP_Easy7/E7_8.rb
@@ -1,1 +1,27 @@
-multiply_all_pairs([2, 4], [4, 3, 1, 2]) == [2, 4, 4, 6, 8, 8, 12, 16]
+def multiply_all_pairs(arr1, arr2)
+  products = []
+
+  arr1.each do |num1|
+    arr2.each do |num2|
+      products << num1 * num2
+    end
+  end
+  products.sort
+end
+
+
+# my other solution
+
+def multiply_all_pairs2(arr1, arr2)
+  products = []
+  count = 0
+
+  while count < arr1.size
+    products << arr2.map do |num|
+                  num * arr1[count]
+                end
+    count += 1
+  end
+
+  products.flatten.sort
+end
