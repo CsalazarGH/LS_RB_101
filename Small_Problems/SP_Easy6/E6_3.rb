935674857a4553bdd47c6af752aaa994db413e4a
Chris Salazar <chrissalazarem@icloud.com>
2020-05-08 21:51:02 -0700

    Easy 6 Prob 3 refactored

--- Small_Problems/SP_Easy6/E6_3.rb
+++ Small_Problems/SP_Easy6/E6_3.rb
@@ -1,1 +1,20 @@
+def find_fibonacci_index_by_length(int)
+  hit = false
+  numbers = [1, 1]
+  last_num = 0
 
+  while hit == false
+    numbers << numbers[-2..-1].sum
+    last_num = numbers.last
+    
+    if last_num.to_s.length == int
+      hit = true
+    end
+  end
+
+  numbers.find_index(last_num) + 1
+end
+
+
+
+p find_fibonacci_index_by_length(2)
