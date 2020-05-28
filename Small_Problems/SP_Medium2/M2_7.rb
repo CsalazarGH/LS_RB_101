52213a280d2a3713240f10920da28eee450ccaaa
Chris Salazar <chrissalazarem@icloud.com>
2020-05-28 00:00:00 -0700

    M2_7

--- Small_Problems/SP_Medium2/M2_7.rb
+++ Small_Problems/SP_Medium2/M2_7.rb
@@ -4,10 +4,17 @@ You may assume that the year is greater than 1752 (when the United Kingdom adopt
 Gregorian Calendar) and that it will remain in use for the foreseeable future.
 =end
 
-def friday_13th(2015)
+require 'date'
 
+def friday_13th(year)
+  unlucky_count = 0
+  thirteenth = Date.new(year, 1, 13)
+  12.times do
+    unlucky_count += 1 if thirteenth.friday?
+    thirteenth = thirteenth.next_month
+  end
+  unlucky_count
 end
-
 
 friday_13th(2015) == 3
 friday_13th(1986) == 1
